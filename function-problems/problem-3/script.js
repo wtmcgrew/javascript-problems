e092de5ab30a9475e6e599c11fe356a57ba97e53
Whitney McGrew <wt.mcgrew@gmail.com>
2023-05-26 08:22:24 -0500

    JS code for counting letter occurrences in a string. Initial commit for HTML index

---
+++ function-problems/problem-3/script.js
@@ -0,0 +1,32 @@
+// Write a JavaScript function that accepts two arguments,
+// a string and a letter and the function will count
+// the number of occurrences of the specified letter 
+// within the string.
+
+/*
+
+Step 1: Create a function with two parameters inside
+parentheses
+
+Step 2: Loop through the items using length method
+
+Step 3: Return the count
+
+Step 4: Console log the string and pick letter in second
+argument
+
+
+*/  
+
+function numberOfOccurences (str, letter) {
+	let letterCounter = 0;
+	
+	for (let counter = 0; counter < str.length; counter++) {
+		if (str.charAt(counter) == letter) {
+			letterCounter += 1;
+		}
+	}
+	return letterCounter;
+}
+
+console.log(numberOfOccurences ("youtube.com", "o"));
