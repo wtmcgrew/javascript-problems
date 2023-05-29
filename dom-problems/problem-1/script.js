f72bb117f38928af53e8cbe2f8ac0f104ccb9dc1
Whitney McGrew <wt.mcgrew@gmail.com>
2023-05-29 12:31:02 -0500

    Addition of steps. Define of id for P element so JS code can interate to change background color of button

--- dom-problems/problem-1/script.js
+++ dom-problems/problem-1/script.js
@@ -1,3 +1,22 @@
+// Create a basic HTML page with a P element and a button on it.
+// Then, write a JavaScript function to set the background color 
+// of that paragraph using an "onclick" event added to a button in the UI.
+
+/*
+
+Step 1: Define id of P element on HTML document
+
+Step 2: Create button on HTML document and define backgroundColor method
+
+Step 3: On JS document, define the function backgroundColor that will
+iterate the id of "introsection" to change backgroundCOlor
+
+Step 4: Use the style.backgroundColor property to set the background color
+whenever the button is clicked on the HTML document
+
+
+*/
+
 function backgroundColor() {
-	document.getElementByID("color").style.backgroundColor = "blue";
+	document.getElementById("introsection").style.backgroundColor = "blue";
 }
