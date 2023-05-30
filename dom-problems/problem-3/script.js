7601194546f601858d7c1d7ab9fa11198d7b5cd1
Whitney McGrew <wt.mcgrew@gmail.com>
2023-05-30 17:09:07 -0500

    Initial commit. Dropdown list addition to HTML page. JS function addition to JS code

---
+++ dom-problems/problem-3/script.js
@@ -0,0 +1,22 @@
+// Create an HTML page with a dropdown list with three items in it, 
+// then write a JavaScript function that displays the number of items 
+// and displays the item in the list when you select from the dropdown.
+
+/*
+
+Step 1: Create dropdown list with three items on HTML page.
+
+Step 2: Create JS function that will display the number of items
+and display the item in the list. Look up "HTML select onchange"
+
+Step 3: Use a for loop to grab all the text values of each
+option from the Select element. Put them into array, and
+turn array into string to be displayed in HTML
+
+
+*/
+
+
+function creditCardList() {
+	
+}
