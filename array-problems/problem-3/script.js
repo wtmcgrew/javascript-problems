b5665571c90edbb86568b17c5590c214dd1d3242
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-02 22:10:33 -0500

    JS embed to HTML. Sort method and comparison function method to sort numbers on JS

---
+++ array-problems/problem-3/script.js
@@ -0,0 +1,30 @@
+// Define a function that takes in an array of numbers/integers 
+// and returns the sorted version of that array, greatest to least. 
+// (Note: do NOT use the "reverse" method.)
+
+/*
+
+Step 1: Define and initialize an array of integers inside a variable
+
+Step 2: Define and initiliaze a function with array as parameter
+
+Step 3: Find the right array method to sort integers from greatest to least
+
+Step 4: Console log results
+
+*/
+
+const arrayOfNumbers = [8, 3, 2, 9, 4, 7, 1, 6, -9, 20];
+
+function sortedNumbers (arr) {
+	/* Cpmparison function
+	Negative value means a will be before b
+	Positive value means b will be before a
+	If equal to 0 tthey will be right next to each otherr */
+
+	return arrayOfNumbers.sort(function (a, b) {
+		return b - a; 
+	})
+}
+
+console.log(sortedNumbers(arrayOfNumbers));
