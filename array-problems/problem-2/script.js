5cec8b3f3ec191f04093536eabce428fd827db5e
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-02 19:07:05 -0500

    Intial commit

---
+++ array-problems/problem-2/script.js
@@ -0,0 +1,18 @@
+// Define a function that takes in an array of elements and joins them together 
+// into a string separated by a comma with a space after the comma.
+
+/*
+
+Step 1: Define variable and initialize an array of items
+
+Step 2: Create a function that takes in an array as parameter
+
+Step 3: Find the correct property/method that can be used to separate
+into a string separated by a commaa
+
+Step 4: Set up conditional statements and use methods that may break code
+
+Step 5: Console log results
+
+
+*/
