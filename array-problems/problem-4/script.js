657cca4a0e6e82c7db34aa4ce150247af5db024d
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-03 10:25:05 -0500

    Addition of JS script to HTML. For loop to create new array without duplicates

---
+++ array-problems/problem-4/script.js
@@ -0,0 +1,40 @@
+// Write a JavaScript function to remove duplicate numbers 
+// from an array of numbers.
+// NOTE: Do NOT use the Set method.
+
+/*
+
+Step 1: Define/initialize varaible of an array of numbers
+
+Step 2: Define/initialize function with array as paramater
+
+Step 3: Find a method that can be used to remove duplicate numbers
+that do not use the Set method
+
+Step 4: Return value
+
+Step 5: Console log results outside of function
+
+*/
+
+const arrayOfNumbers = [1, 1, 2, 2, 3, 3, 4, 4, 8, 8, 9, 9];
+
+function removeDuplicates (array) {
+	let newArray = []; // Creates empty array
+
+
+    for(let counter = 0; counter < array.length; counter++) {
+    	/*If new array does NOT (!) contain passed through array at counter,
+		add (push) new array element at counter*/
+        if(!newArray.includes(array[counter])) {
+            newArray.push(array[counter]);
+        }
+    }
+    return newArray;
+}
+
+console.log(removeDuplicates(arrayOfNumbers));
+
+
+
+	
