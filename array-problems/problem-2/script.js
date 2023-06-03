1485d312f9b2b40fbc2283ec6e7584d6d7594681
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-03 08:08:36 -0500

    Spell check of JS code

--- array-problems/problem-2/script.js
+++ array-problems/problem-2/script.js
@@ -22,8 +22,8 @@ let cars = ["Chrysler", "Dodge", "Jeep", "Fiat"];
 /* Test to see if program works with different array
 cars = ["Toyota", "Honda", "Hyundai", "Kia", "Mazda"]; */
 
-const stringOfElemnts = function (array) {
+const stringOfElements = function (array) {
 	return array.join(", "); // toString method can be used but was instructed not to
 }
 
-console.log(stringOfElemnts(cars));
+console.log(stringOfElements(cars));
